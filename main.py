10a252b03a5c3c6962f0709362f6568447ff7635
bsrecker <42258510+bsrecker@users.noreply.github.com>
2021-05-14 12:09:15 -0500

    Tkinter Intro

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#Ligme Balls
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+                                               NavigationToolbar2Tk)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# plot function is created for
+# plotting the graph in
+# tkinter window
+def plot():
+    # the figure that will contain the plot
+    fig = Figure(figsize=(5, 5),
+                 dpi=100)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # list of squares
+    y = [i ** 2 for i in range(101)]
+
+    # adding the subplot
+    plot1 = fig.add_subplot(111)
+
+    # plotting the graph
+    plot1.plot(y)
+
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,
+                               master=window)
+    canvas.draw()
+
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,
+                                   window)
+    toolbar.update()
+
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+
+# the main Tkinter window
+window = Tk()
+
+# setting the title
+window.title('Plotting in Tkinter')
+
+# dimensions of the main window
+window.geometry("500x500")
+
+# button that displays the plot
+plot_button = Button(master=window,
+                     command=plot,
+                     height=2,
+                     width=10,
+                     text="Plot")
+
+# place the button
+# in main window
+plot_button.pack()
+
+# run the gui
+window.mainloop()
